b09658fc8f5e2849aeadb23982a4aa044afecb24
Jagratimishra02 <nishimisra002@gmail.com>
2026-08-11 10:06:21 +0530

    Time: 0 ms (100%), Space: 43.7 MB (63.7%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,29 +1,18 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int [] c = new int [m+n];
-        Mergetwo(c ,nums1, m,nums2,n);
-         for(int idx = 0; idx < m+n; idx++){
-         nums1[idx] = c[idx];
-    }
-        for(int i : c){
-            System.out.print(i);
+        int i = m -1 ;
+        int j = n -1 ;
+        int k = m + n -1 ;
+        while(i >= 0 && j >= 0){ 
+            if(nums1[i] > nums2[j]){
+            nums1[k--] = nums1[i--];
+        } else {
+            nums1[k--] = nums2[j--];
+         }
         }
-         System.out.println();
-    }
-    public void Mergetwo(int [] c ,int[] nums1, int m, int[] nums2, int n){
-        int i = 0,j = 0,k = 0;
-        while(i < m && j  < n){
-            if(nums1[i]<nums2[j]){
-              c[k++] = nums1[i++];  
-            } else {
-              c[k++] = nums2[j++];
-            }
+       
+        while(j>=0){
+             nums1[k--] = nums2[j--];
         }
-        while(i < m){
-            c[k++] = nums1[i++];
-        } 
-        while(j < n){
-            c[k++] = nums2[j++];
-        } 
     }
 }
