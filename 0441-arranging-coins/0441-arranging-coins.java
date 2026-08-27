92c34fcbed88e1eb8fd65a7fcd3869ca7fb5f78e
Jagratimishra02 <nishimisra002@gmail.com>
2026-08-27 23:42:20 +0530

    Time: 1 ms (100%), Space: 42.5 MB (81.01%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        long start = 0 ;
+        long end =  n;
+        long ans = 0;
+        while(start<=end){
+            long k = start+(end-start)/2;
+            long m =k*(k+1)/2;
+            if(m == n) return (int)k;
+            else if(m<n) {
+                ans = k ;
+                start = k+1;
+            }
+            else end = k -1 ;
+        }
+        return (int)end;
+    }
+}
