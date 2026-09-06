82ccb62f23f113b9d3eb9b4112a549275460bbc6
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-06 21:57:25 +0530

    Time: 0 ms (100%), Space: 43.1 MB (43.84%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -6,6 +6,41 @@ class Solution {
        int LastRow = m-1;
        int firstCol = 0;
        int LastCol = n-1;
+
+       // method 1
+    //    int tne = m*n;
+    //    while(ans.size()<tne){
+
+    //     // for right 
+    //     for(int i = firstCol ; i <= LastCol; i++){
+    //         ans.add(matrix[firstRow][i]);
+    //     }
+    //     firstRow++;
+    //     if(ans.size()==tne) break;   // if first  row or col is greater then row or col break.
+
+    //     // for down
+    //     for(int j = firstRow ; j <= LastRow ; j++){
+    //         ans.add(matrix[j][LastCol]);
+    //     }
+    //     LastCol--;
+    //     if(ans.size()==tne) break;
+
+    //     // for left
+    //     for(int i = LastCol ; i >= firstCol ; i--){
+    //         ans.add(matrix[LastRow][i]);
+    //     }
+    //     LastRow--;
+    //     if(ans.size()==tne) break;
+
+    //     // for up
+    //     for(int j = LastRow ; j >= firstRow ; j--){
+    //         ans.add(matrix[j][firstCol]);
+    //     }
+    //     firstCol++;
+    //     if(ans.size()==tne) break;
+    //    }
+
+    // method 2
        while(firstRow<=LastRow && firstCol <= LastCol){
 
         // for right 
